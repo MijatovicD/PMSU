85536434d9cfb48b188d2a8d7933c2fb8ffc6467
Dimitrije Mijatovic <mijatovicdimitrije@gmail.com>
2018-03-26 18:45:47 +0200

    Napravljen layout za read i create posts

--- app/src/main/java/com/example/dimitrije/pmsu/ReadPostActivity.java
+++ app/src/main/java/com/example/dimitrije/pmsu/ReadPostActivity.java
@@ -2,6 +2,7 @@ package com.example.dimitrije.pmsu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReadPostActivity extends AppCompatActivity {
 
@@ -9,6 +10,30 @@ public class ReadPostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_post);
+
+        TextView titleText = findViewById(R.id.title_Read);
+        titleText.setText("Naslov posta");
+
+        TextView descriptionnText = findViewById(R.id.description_Read);
+        descriptionnText.setText("Opis posta");
+
+        TextView userText = findViewById(R.id.user_Read);
+        userText.setText("pera");
+
+        TextView dateText = findViewById(R.id.date_Read);
+        dateText.setText("26-3-2018");
+
+        TextView locationText = findViewById(R.id.location_Read);
+        locationText.setText("Novi Sad");
+
+        TextView likeText = findViewById(R.id.like_Read);
+        likeText.setText("Likes 8");
+
+        TextView dislikeText = findViewById(R.id.dislike_Read);
+        dislikeText.setText("Dislikes 4");
+
+        TextView tagText = findViewById(R.id.tag_Read);
+        tagText.setText("#hashtag");
     }
 
     @Override
